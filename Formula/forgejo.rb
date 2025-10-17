41e33e126e42d633a896b50ff9bb898c431a4322
John Faust <45057319+slickag@users.noreply.github.com>
2025-10-17 03:07:12 +0000

    forgejo 13.0.0, rclone 1.71.1 & taproom 0.4.5 (new formulas)

---
+++ Formula/forgejo.rb
@@ -0,0 +1,51 @@
+class Forgejo < Formula
+  desc "Self-hosted lightweight software forge"
+  homepage "https://forgejo.org/"
+  url "https://codeberg.org/forgejo/forgejo/releases/download/v13.0.0/forgejo-src-13.0.0.tar.gz"
+  sha256 "8d38f4e1d801d9cec79bddc42b2c77216d7ab1cb077a781abe44b82359915f39"
+  license "GPL-3.0-or-later"
+  head "https://codeberg.org/forgejo/forgejo.git", branch: "forgejo"
+
+  depends_on "go" => :build
+  depends_on "node" => :build
+
+  uses_from_macos "sqlite"
+
+  def install
+    ENV["CGO_ENABLED"] = "1"
+    ENV["TAGS"] = "bindata timetzdata sqlite sqlite_unlock_notify"
+    system "make", "build"
+    system "go", "build", "contrib/environment-to-ini/environment-to-ini.go"
+    bin.install "gitea" => "forgejo"
+    bin.install "environment-to-ini"
+  end
+
+  service do
+    run [opt_bin/"forgejo", "web", "--work-path", var/"forgejo"]
+    keep_alive true
+    log_path var/"log/forgejo.log"
+    error_log_path var/"log/forgejo.log"
+  end
+
+  test do
+    ENV["FORGEJO_WORK_DIR"] = testpath
+    port = free_port
+
+    pid = fork do
+      exec bin/"forgejo", "web", "--port", port.to_s, "--install-port", port.to_s
+    end
+    sleep 5
+    sleep 10 if OS.mac? && Hardware::CPU.intel?
+
+    output = shell_output("curl -s http://localhost:#{port}/api/settings/api")
+    assert_match "Go to default page", output
+
+    output = shell_output("curl -s http://localhost:#{port}/")
+    assert_match "Installation - Forgejo: Beyond coding. We Forge.", output
+
+    assert_match version.to_s, shell_output("#{bin}/forgejo -v")
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
